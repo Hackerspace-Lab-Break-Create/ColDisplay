9d2910f300a28fe72f0c2644a43df7c87dbf9b1c
Kevin <kevind@backbase.com>
2021-02-16 22:27:37 -0100

    basic buffer with set pixels

--- ColDisplay/ColDisplay.h
+++ ColDisplay/ColDisplay.h
@@ -3,12 +3,110 @@
 
 #include <Vector.h>
 
+const static int MAX_WIDTH = 10;  //BYTES
+const static int MAX_HEIGHT = 8; //LEDS
 
-const int ELEMENT_COUNT_MAX = 50;
 
-class RowDisplay{
+class ColDisplay{
+  private:
+    byte displayBuffer[MAX_HEIGHT][MAX_WIDTH];
+    int pixelHeight = MAX_HEIGHT; //Number of leds verticaly 
+    int viewWidthNumBytes = MAX_WIDTH; //Number of bytes in view
+      
   public:
+
+    ColDisplay (){
+      fillBuffer(0b00000000);
+    }
     
+    void updateLoop ();
+    void printDisplay();
+    void setPixel (int x, int y, bool val);
+    void fillBuffer (byte val);
+    void fillView ();
+    void setBytes (int x, int y, byte* val, int height, int width);
+};
+
+
+void ColDisplay::setBytes (int x, int y, byte* val, int height, int width){
+  if (x < 0 || (x/8) > MAX_WIDTH || y < 0 || y > MAX_HEIGHT-1){
+    return;
+  }
+//  Serial.println(*val,BIN);
+//  Serial.println("===");
+  for (int i = 0; i < height; i++){
+    for (int j = 0; j < width; j++){
+     
+      byte current = *((val+i*width)+j);
+      
+      //Serial.print(current,BIN);
+      
+      for (int m = 0; m < 8; m++){
+        int xa = ((x+j)+m +j*8)+x;
+        if ( (xa/8) > MAX_WIDTH-1){
+          break;
+        }
+        int ya = y+i;
+        
+//        Serial.print ("\n\nX,Y, v: ");
+//        Serial.print(xa);
+//        Serial.print(",");
+//        Serial.print(ya);
+//        Serial.print(",");
+//        Serial.println((7-m));
+        setPixel (xa,ya,bitRead(current,7-m));
+      }
+      //Serial.print (",");
+    }
+    //Serial.println(" ");
+  }
+
+
+  
+}
+
+void ColDisplay::fillBuffer (byte val){
+  for (int i = 0; i < MAX_HEIGHT; i++){
+        for (int j = 0; j < MAX_WIDTH; j++){
+          displayBuffer[i][j] = val;  
+        }
+      }
+
+}
+void ColDisplay::setPixel (int x, int y, bool val){
+  if( x < 0 || (x/8) > MAX_WIDTH || y < 0 || y > MAX_HEIGHT-1){
+    return;
+  }
+//  Serial.print("\nSetting pixel x,y,val: ");
+//  Serial.print(x);
+//  Serial.print(",");
+//  Serial.print(y);
+//  Serial.print(",");
+//  Serial.print(val);
+//  Serial.print("\n");
+  bitWrite(displayBuffer[y][(x/8)],(8-(x%8))-1,val);
+}
+
+
+void ColDisplay::printDisplay (){
+  Serial.println("");
+  for (int i = 0; i < pixelHeight; i++){
+    Serial.println("");
+    for (int j = 0; j < viewWidthNumBytes; j++){
+      for(int k =1; k <= 8; k++){
+        Serial.print(bitRead(displayBuffer[i][j],8-k));
+      } 
+    }
+  }
+}
+
+
+void ColDisplay::updateLoop(){
+  digitalWrite(13,LOW);
+   delay (500);
+
+   digitalWrite(13,HIGH);
+   delay(500);
 
 }
 
--- ColDisplay/ColDisplay.ino
+++ ColDisplay/ColDisplay.ino
@@ -1,13 +1,79 @@
+#include "ColDisplay.h"
+
+
+ColDisplay colDisplay;
+float roundTripTime = 15;//1.8*4; //// Time (Hz) for 1 cycle. 1 complete buffer push + remaining space (total steps in view size)
+
+const int VIEW_SIZE = 8;
+const int TOTAL_STEPS = VIEW_SIZE * 8;
+
+void configureTimer () {
+  cli();
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  // Divide by the size of buffer length to get calls each time step
+  //OCR1A = 15624/BUFFER_LENGTH
+  // = (16*10^6) / (1*1024) - 1 (must be <65536)
+
+  OCR1A = (16000000 / ((roundTripTime) * 1024) - 1) / TOTAL_STEPS;
+  //OCR1A = (16000000 / ((roundTripTime) * 1024) - 1);
+
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  sei();
+
+}
+
 void setup() {
-   pinMode (13, OUTPUT);
+
+  //configureTimer();
+  Serial.begin(115200);
+  colDisplay.printDisplay();
+
+  // A
+  byte letter[][2] = 
+      {
+        {0b01110000,0b11110000},
+        {0b10001000,0b11110000},
+        {0b10001000,0b11110000},
+        {0b11111000,0b11110000},
+        {0b10001000,0b11110000},
+        {0b10001000,0b11110000},
+        {0b10001000,0b11110000},
+        {0b00000000,0b11110000}
+      };
+
+    for(int i = 0; i < 40;i++){
+      colDisplay.fillBuffer(0b00000000);
+      colDisplay.setBytes (i,0,(byte*)letter,8,2);
+      colDisplay.printDisplay ();  
+    }
+ 
+
+    colDisplay.setPixel (0,0,true);
+    colDisplay.printDisplay();
+  // "clock" led so we can see each pulse of the time step
+  pinMode (13, OUTPUT);
+  pinMode (2, OUTPUT);
+  pinMode (3, OUTPUT);
+  pinMode (4, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (7, OUTPUT);
+  pinMode (8, OUTPUT);
+  pinMode (9, OUTPUT);
 
 }
 
 void loop() {
-   digitalWrite(13,LOW);
-   delay (500);
-
-   digitalWrite(13,HIGH);
-   delay(500);
-
+  
+  
+ 
+   colDisplay.updateLoop();
 }
